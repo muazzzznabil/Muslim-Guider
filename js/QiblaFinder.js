8b4c084452c1125ac414ebb4cc4f8281b3d378e4
muazzzznabil <152073149+muazzzznabil@users.noreply.github.com>
2024-05-29 00:49:49 +0800

    separate js

---
+++ js/QiblaFinder.js
@@ -0,0 +1,13 @@
+function getQiblaDirection() {
+
+    event.preventDefault();
+    var lat = document.getElementById("latitude").value;
+    var lng = document.getElementById("longitude").value;
+
+    var url = 'https://api.aladhan.com/v1/qibla/' + lat + '/' + lng;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("qibla-direction").innerHTML = "Qibla direction: " + data.data.direction;
+        });
+}
